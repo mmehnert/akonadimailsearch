3b50d7cef69234df4f8be48309841030db901c46
Maximilian Mehnert <maximilian.mehnert@gmx.de>
2013-10-27 15:15:17 +0100

    copyright

--- src/akonadimailsearch.cpp
+++ src/akonadimailsearch.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2013 by  <>                 *
+ *   Copyright (C) 2013 by  Maximilian Mehnert <maximilian.mehnert@gmx.de> *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
